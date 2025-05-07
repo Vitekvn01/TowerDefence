4a8dc8582db3f17e9fb94e3614397cb530d5d097
vitekvn01@yandex.ru <vitekvn01@yandex.ru>
2025-05-07 21:19:45 +0700

    Add waves in EnemySpawner

--- Assets/Enemy/AIEnemy.cs
+++ Assets/Enemy/AIEnemy.cs
@@ -6,20 +6,47 @@ using UnityEngine.AI;
 public class AIEnemy : MonoBehaviour
 {
     private NavMeshAgent _agent;
-    public Enemy Enemy { get; private set; }
     
+    public Enemy Enemy { get; private set; }
     public EnemyStateMachine FSM { get; private set; }
 
     private void Awake()
     {
-        Enemy = GetComponent<Enemy>();
         _agent = GetComponent<NavMeshAgent>();
-
+        Enemy = GetComponent<Enemy>();
+        
         FSM = new EnemyStateMachine(this);
     }
     
     private void OnEnable()
     {
+        _agent.Resume();
+        
+        Debug.Log($"[{name}] OnEnable start");
+    
+        // 1. Перенести агента внутрь NavMesh
+        if (NavMesh.SamplePosition(transform.position, out var hit, 2f, NavMesh.AllAreas))
+        {
+            _agent.enabled = false;
+            _agent.Warp(hit.position);
+            _agent.enabled = true;
+        }
+        else Debug.LogWarning($"[{name}] Spawn off NavMesh!");
+    
+        // 2. Сброс остановки и пути
+        _agent.isStopped = false;
+        _agent.updatePosition = true;
+        _agent.updateRotation = true;
+        
+    
+        // 4. Лог финального состояния
+        Debug.Log($"[{name}] OnEnable end — isOnNavMesh={_agent.isOnNavMesh}, dest={_agent.destination}");
+        
+        Vector3 pos = transform.position;
+        bool onMesh = NavMesh.SamplePosition(pos, out var hit2, 2f, NavMesh.AllAreas);
+        Debug.DrawRay(pos, Vector3.up * 2f, onMesh ? Color.green : Color.red, 5f); 
+        Debug.Log($"SamplePosition returned {onMesh}, hit.pos={hit2.position}");  // :contentReference[oaicite:0]{index=0}
+        
         TryInitializeAI();
     }
     
@@ -68,12 +95,21 @@ public class AIEnemy : MonoBehaviour
     
     private void TryInitializeAI()
     {
+        _agent.enabled = false;
+        _agent.enabled = true;
+            
+        _agent.isStopped = false;
+        
         if (Enemy.Target != null)
         {
             SetTargetMove(Enemy.Target);
             SetSpeedMove(Enemy.Speed);
             
             FSM.ChangeState(new MoveToTargetState(FSM));
+        }
+        else
+        {
+            Debug.Log("Enemy.Target == null");
         }
     }
 
--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -6,33 +6,63 @@ using UnityEngine.Serialization;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private int _count;
-    [SerializeField] private float _spawnDelay;
+    [SerializeField] private List<Wave> _waves;
 
     [SerializeField] private Transform _spawnPoint;
     [SerializeField] private Transform _target;
-
-    [FormerlySerializedAs("enemyUnitPrefab")] [FormerlySerializedAs("_enemyPrefab")] [SerializeField] private Enemy enemyPrefab;
+    
+    [SerializeField] private Enemy _enemyPrefab;
 
     private ObjectPool _enemyPool;
+    
+    private Wave _currentWave;
+    private int _currentWaveNumber = 0; 
+    private int _spawned;
+    private bool _isAllSpawned = false;
+
+    private List<Enemy> _spawnEnemies = new List<Enemy>();
+    public event Action AllEnemySpawnedEvent;
+    public event Action AllEnemyDeadEvent;
+    public event Action<int, int> EnemyCountChanged;
 
     private void Awake()
     {
-        _enemyPool = new ObjectPool(enemyPrefab.gameObject, 50);
+        _enemyPool = new ObjectPool(_enemyPrefab.gameObject, 50);
     }
 
     private void Start()
     {
+        AllEnemyDeadEvent += NextWave;
+        SetWave(0);
         StartCoroutine(SpawnEnemiesRoutine());
     }
-
+    
+    public void NextWave() 
+    {
+        SetWave(++_currentWaveNumber);
+        _spawned = 0;
+        StartCoroutine(SpawnEnemiesRoutine());
+    }
+    
+    private void SetWave(int index)
+    {
+        _isAllSpawned = false;
+        _currentWave = _waves[index];
+        EnemyCountChanged?.Invoke(0, 1);
+    }
+    
     private IEnumerator SpawnEnemiesRoutine()
     {
-        for (int i = 0; i < _count; i++)
+        for (int i = 0; i < _currentWave.Count; i++)
         {
-            SpawnEnemy(_spawnPoint.position);
-            yield return new WaitForSeconds(_spawnDelay);
+            Enemy spawnedEnemy = SpawnEnemy(_spawnPoint.position);
+            _spawned++;
+            yield return new WaitForSeconds(_currentWave.Delay);
         }
+
+        _isAllSpawned = true;
+        AllEnemySpawnedEvent?.Invoke();
+        EnemyCountChanged?.Invoke(_spawned, _currentWave.Count);
     }
 
     private Enemy SpawnEnemy(Vector3 position)
@@ -42,9 +72,41 @@ public class EnemySpawner : MonoBehaviour
         Enemy enemy = spawned.GetComponent<Enemy>();
         enemy.transform.position = position;
         enemy.gameObject.SetActive(true);
-
         enemy.Initialize(_target);
+        
+        _spawnEnemies.Add(enemy);
+        enemy.OnDeadEvent += OnEnemyDead;
+        
+
 
         return enemy;
     }
+    
+    private void OnEnemyDead(Enemy enemy)
+    {
+        enemy.OnDeadEvent -= OnEnemyDead;
+        _spawnEnemies.Remove(enemy);
+        ChangeSpawnedEnemy();
+    }
+
+    private void ChangeSpawnedEnemy()
+    {
+        _spawned -= 1;
+        
+        if (_spawned <= 0)
+        {
+            if (_isAllSpawned)
+            {
+                AllEnemyDeadEvent?.Invoke();
+            }
+        }
+    }
+}
+
+[Serializable]
+public class Wave
+{
+    public GameObject Template;
+    public int Count;
+    public int Delay;
 }
