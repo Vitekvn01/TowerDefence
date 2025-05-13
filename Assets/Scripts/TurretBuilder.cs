352fd5d7c903360fe41bc85c4542ad0925cf6293
vitekvn01@yandex.ru <vitekvn01@yandex.ru>
2025-05-13 16:30:24 +0700

    Add ShopView

--- Assets/Scripts/TurretBuilder.cs
+++ Assets/Scripts/TurretBuilder.cs
@@ -1,4 +1,3 @@
-using System;
 using AbstractFaсtory;
 using UnityEngine;
 
--- Assets/Scripts/UI/Shop.cs
+++ Assets/Scripts/UI/Shop.cs
@@ -1,60 +1,40 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Shop : MonoBehaviour
 {
     [SerializeField] private List<TurretData> _turretDatas;
-    [SerializeField] private TurretView _turretViewPrefab;
-    [SerializeField] private GameObject _itemConteiner;
     [SerializeField] private TurretBuilder _turretBuilder;
-    
-    private List<TurretView> _turretViews = new List<TurretView>();
+    [SerializeField] private ShopView _shopView;
+
+    private void Awake()
+    {
+        _shopView.OnBuyRequested += TrySellTurret;
+    }
 
     private void Start()
     {
-        for (int i = 0; i < _turretDatas.Count; i++)
-        {
-            AddTurret(_turretDatas[i]);
-        }
+        _shopView.Render(_turretDatas);
     }
 
     private void OnDestroy()
     {
-        foreach (var view in _turretViews)
-        {
-            if (view != null)
-            {
-                view.SellButtonClick -= OnSellButtonClick;
-            }
-        }
-    }
-
-    private void AddTurret(TurretData turretData)
-    {
-        var view = Instantiate(_turretViewPrefab, _itemConteiner.transform);
-        view.SellButtonClick += OnSellButtonClick;
-        view.OnDestroyEvent += OnDestroyView;
-        view.Render(turretData);
-        _turretViews.Add(view);
+        _shopView.OnBuyRequested -= TrySellTurret;
     }
 
     private void OnSellButtonClick(TurretData turretData)
     {
         TrySellTurret(turretData);
     }
-
-    private void OnDestroyView(TurretView view)
-    {
-        _turretViews.Remove(view);
-        view.SellButtonClick -= OnSellButtonClick;
-    }
+    
 
     private void TrySellTurret(TurretData turretData)
     {
         if (turretData.Price <= 1000)
         {
             _turretBuilder.StartPlacement(turretData);
+            _shopView.Hide();
         }
     }
 }
