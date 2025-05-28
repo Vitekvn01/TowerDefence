e1893a42102a4b331e22f7c76418d31204c94f28
vitekvn01@yandex.ru <vitekvn01@yandex.ru>
2025-05-28 21:16:21 +0700

    Add Appodeal

---
+++ Assets/Original/Scripts/ADController.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+using AppodealAds.Unity.Api;
+using AppodealAds.Unity.Common;
+
+public class ADController : IInterstitialAdListener, IAppodealInitializationListener
+{
+    private const string AppKey = "1c1e57cb3cdae0b3afde949eafc56230ea3132a142e0b6cb";
+    
+    public ADController()
+    {
+        int adTypes = Appodeal.INTERSTITIAL;
+
+        Appodeal.initialize(AppKey, adTypes, this);
+
+        Appodeal.setInterstitialCallbacks(this);
+
+    }
+
+    public void ShowInterstitial()
+    {
+        if(Appodeal.isLoaded(Appodeal.INTERSTITIAL)) {
+            Appodeal.show(Appodeal.INTERSTITIAL);
+        }
+    }
+    public void onInterstitialLoaded(bool isPrecache)
+    {
+        Debug.Log("InterstitialLoaded");
+    }
+
+    public void onInterstitialFailedToLoad()
+    {
+        Debug.Log("InterstitialFailedToLoad");
+    }
+
+    public void onInterstitialShowFailed()
+    {
+        Debug.Log("InterstitialShowFailed");
+    }
+
+    public void onInterstitialShown()
+    {
+        Debug.Log("InterstitialShown");
+    }
+
+    public void onInterstitialClosed()
+    {
+        Debug.Log("InterstitialClosed");
+    }
+
+    public void onInterstitialClicked()
+    {
+        Debug.Log("InterstitialClicked");
+    }
+
+    public void onInterstitialExpired()
+    {
+        Debug.Log("InterstitialExpired");
+    }
+
+    public void onInitializationFinished(List<string> errors)
+    {
+        Debug.Log("InitializationFinished");
+
+        if (errors.Count != 0)
+        {
+            int index = 1;
+            foreach (var error in errors)
+            {
+                Debug.Log($"{index}_error: {error}");
+            }
+        }
+    }
+}
--- Assets/Original/Scripts/Bootstrapper.cs
+++ Assets/Original/Scripts/Bootstrapper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using IInterfaces;
 using UnityEngine;
@@ -8,13 +9,20 @@ public class Bootstrapper : MonoBehaviour
     [SerializeField] private List<Level> _levels;
     
     private ILevelManager _levelManager;
+    private ADController _adController;
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
-        
+        _adController = new ADController();
         _levelManager = new LevelManager(_levels, _firstScene);
+        _levelManager.OnLoadLevelEvent += _adController.ShowInterstitial;
         ServiceLocator.Register(_levelManager);
 
         _levelManager.LoadFirstScene();
+    }
+
+    private void OnDestroy()
+    {
+        _levelManager.OnLoadLevelEvent -= _adController.ShowInterstitial;
     }
 }
